bbf5a92cd580a367faaa904e3c8b1b84a840e751
winstonisac <105824247+winstonisac@users.noreply.github.com>
2022-11-22 21:30:56 -0300

    Aula 2 Obstacle Manager

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,13 +1,15 @@
 import pygame
+from pygame.sprite import Sprite
+
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING 
 
 X_POS = 80
 Y_POS = 310
-Y_POS_DUCK = 340
+Y_POS_DUCK = 340 
 JUMP_VEL = 8.5
 
 
-class Dinosaur: 
+class Dinosaur(Sprite): 
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -17,7 +19,7 @@ class Dinosaur:
         self.jump_vel = 8.5
         self.dino_run = True
         self.dino_jump = False
-        self.dino_duck = False
+        self.dino_duck = False 
 
     
     def run(self):
@@ -33,16 +35,16 @@ class Dinosaur:
             self.jump_vel -= 0.8
 
         if self.jump_vel < -JUMP_VEL:  
-            #self.dino_rect.x = Y_POS
             self.dino_jump = False
             self.jump_vel = JUMP_VEL 
 
-    def duck(self):
+    def duck(self): 
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS_DUCK
         self.step_index += 1
+        self.dino_duck = False
 
 
     def update(self, user_input):
@@ -53,16 +55,15 @@ class Dinosaur:
         elif self.dino_duck:
             self.duck()
 
-
-        if user_input[pygame.K_UP] and not self.dino_jump:
+        if user_input[pygame.K_UP] and not self.dino_jump: 
             self.dino_duck = False
             self.dino_run = False
             self.dino_jump = True
-        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+        elif user_input[pygame.K_DOWN] and not self.dino_jump: 
             self.dino_duck = True
             self.dino_run = False
             self.dino_jump = False
-        elif not user_input[pygame.K_DOWN] and not user_input[pygame.K_UP] and not self.dino_jump: #erro validação
+        elif not self.dino_jump and not self.dino_duck: 
             self.dino_duck = False
             self.dino_run = True
             self.dino_jump = False
